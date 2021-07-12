e4e8fd0889df0d2316c4101890383b2816463cb6
Mounica lakshmi <60734309+git170060024@users.noreply.github.com>
2021-07-12 19:07:50 +0530

    Create calender.h

---
+++ 4_Implementation/calender.h
@@ -0,0 +1,25 @@
+/** 
+* @file calendar.h
+* 
+*
+*/
+#ifndef __CALENDAR_H__
+#define __CALENDAR_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+char *month[]={"January","February","March","April","May","June","July","August","September","October","November","December"};
+
+
+char *week[]={"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+
+
+int days[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+
+
+    int tdays=0,k,myear;
+
+#endif  /* #define __CALENDAR_H__*/
