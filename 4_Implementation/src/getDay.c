31736570d6b6dd3bedd548ec036e52d0645b0ad9
Mounica lakshmi <60734309+git170060024@users.noreply.github.com>
2021-07-12 23:59:10 +0530

    Create getDay.c

---
+++ 4_Implementation/src/getDay.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"func.h"
+char *getDay(int dd,int mm,int yy){
+    int day;
+    if(!(mm>=1 && mm<=12)){
+        return("Invalid month value");
+    }
+    if(!(dd>=1 && dd<=getNumberOfDays(mm,yy))){
+        return("Invalid date");
+    }
+    if(yy>=1600){
+        day = getDayNumber(dd,mm,yy);
+        day = day%7;
+        return(getName(day));
+    }else{
+        return("Please give year more than 1600");
+    }
+}
